c8da59f895a7339876134b0d4c9e780f86722d62
Kalin Bowden <KalinBowden@gmail.com>
2017-09-16 18:03:51 -0500

    First commit

---
+++ cafe.js
@@ -0,0 +1,43 @@
+function changeLink1()
+{
+  var text = document.getElementById("link1").style.background = "linear-gradient( 180deg, #303030	, #1a75ff)";
+  var bord = document.getElementById("link1").style.borderWidth = "medium";
+  var push = document.getElementById("link1").style.paddingTop = "27%";
+}
+
+function changeLink1Back()
+{
+  var text = document.getElementById("link1").style.background = "none";
+  var bord = document.getElementById("link1").style.borderWidth = "0";
+  var push = document.getElementById("link1").style.paddingTop = "30%";
+}
+
+function changeLink2()
+{
+  var text = document.getElementById("link2").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+}
+
+function changeLink2Back()
+{
+  var text = document.getElementById("link2").style.background = "none";
+}
+
+function changeLink3()
+{
+  var text = document.getElementById("link3").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+}
+
+function changeLink3Back()
+{
+  var text = document.getElementById("link3").style.background = "none";
+}
+
+function changeLink4()
+{
+  var text = document.getElementById("link4").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+}
+
+function changeLink4Back()
+{
+  var text = document.getElementById("link4").style.background = "none";
+}
