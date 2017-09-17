b3e7401bfee486679839c092551e561d403a5e50
Kalin Bowden <KalinBowden@gmail.com>
2017-09-17 16:36:39 -0500

    Updated js and error checking

--- cafe.js
+++ cafe.js
@@ -1,38 +1,46 @@
-function changeLink1()
+function changeLink(pic)
 {
-  var text = document.getElementById("link1").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+  if (pic === 0)
+  {
+    var text = document.getElementById("link1").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+  }
+  else if (pic === 1)
+  {
+    var text = document.getElementById("link2").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+  }
+  else if (pic === 2)
+  {
+    var text = document.getElementById("link3").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+  }
+  else if (pic === 3)
+  {
+    var text = document.getElementById("link4").style.background = "linear-gradient( 0deg, #303030	, #888888)";
+  }
 }
 
-function changeLink1Back()
+function changeBack(pic)
 {
-  var text = document.getElementById("link1").style.background = "none";
-}
-function changeLink2()
-{
-  var text = document.getElementById("link2").style.background = "linear-gradient( 0deg, #303030	, #888888)";
-}
-
-function changeLink2Back()
-{
-  var text = document.getElementById("link2").style.background = "none";
-}
-
-function changeLink3()
-{
-  var text = document.getElementById("link3").style.background = "linear-gradient( 0deg, #303030	, #888888)";
-}
-
-function changeLink3Back()
-{
-  var text = document.getElementById("link3").style.background = "none";
+  if (pic === 0)
+  {
+    var text = document.getElementById("link1").style.background = "none";
+  }
+  else if (pic === 1)
+  {
+    var text = document.getElementById("link2").style.background = "none";
+  }
+  else if (pic === 2)
+  {
+    var text = document.getElementById("link3").style.background = "none";
+  }
+  else if (pic === 3)
+  {
+    var text = document.getElementById("link4").style.background = "none";
+  }
 }
 
-function changeLink4()
+function logIn()
 {
-  var text = document.getElementById("link4").style.background = "linear-gradient( 0deg, #303030	, #888888)";
-}
-
-function changeLink4Back()
-{
-  var text = document.getElementById("link4").style.background = "none";
+  var email = document.getElementById("email0").value;
+  var password = document.getElementById("password0").value;
+  alert(email + " " + password);
 }
