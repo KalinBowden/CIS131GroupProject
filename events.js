6f88ffaab21389d20261f96635f9bce93938e489
Justin Mangan <jm0015052@otc.edu>
2017-10-01 15:57:31 -0500

    made js for going and maybe buttons

--- events.js
+++ events.js
@@ -1,5 +1,9 @@
 //events.js
-
-function response() {
-	alert("We'll see you there!");
+var i;
+function response(i) {
+	document.getElementById("msg" + (i)).innerHTML="You are going to this event.";
+	//alert("We'll see you there!");
+}
+function response2(i) {
+	document.getElementById("msg" + (i)).innerHTML="We hope to see you there. Tell your friends!";
 }
